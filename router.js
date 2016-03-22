702f04e03213b40b4b5107488270498de4a33f2d
Carlos Danna <carlos.danna@gmail.com>
2016-03-22 13:26:58 -0400

    Post method added

--- router.js
+++ router.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 module.exports = function(app){
   app.route('/data')
+    //testing get api call
     .get(function(req,res){
       var response = {
-        Test: "It is working",
-        Description: 2
+        "Test": "It is working",
+        "Description": 2
       };
+      res.send(response);
+    })
+
+    //Testing post api call
+    .post(function(req,res){
+      console.log(req.body);
+      var response = req.body;
       res.send(response);
     });
 }
