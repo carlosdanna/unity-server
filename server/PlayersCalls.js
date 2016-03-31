a59fc79d22e803b627fc7f9fc557d6a5aa4ddaf7
Carlos Danna <carlos.danna@gmail.com>
2016-03-31 13:07:24 -0400

    adding more functionalities

--- server/PlayersCalls.js
+++ server/PlayersCalls.js
@@ -2,12 +2,41 @@ var mongoose = require('mongoose');
 var Players = require('../models/Players');
 
 module.exports = {
-  insertPlayer : function(_player){
+  insertPlayer : function(req, res){
+    var response = {};
+    var _player = req.body;
     var db = new Players;
     for(var member in _player){
       db[member] = _player[member];
     }
-    console.log(db);
+
+    response = db.save(function(err,data){
+      if (err)
+      {
+        response = {error: true, data: "Something really bad happened"};
+        res.json(response);
+      }
+      else{
+        response = {error: false, data: "Data saved successfully"};
+        res.json(response);
+      }
+    });
+
+  },
+
+  getPlayers : function(req, res){
+    var response ={};
+    Players.find({},function(err,data){
+      if (err){
+        response = {error: true, data: "Something really bad happened"};
+        res.json(response);
+      }
+      else{
+        response = {error: false, data: data};
+        res.json(response);
+      }
+    });
+
   }
 
 };
--- server/router.js
+++ server/router.js
@@ -1,27 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var playerCalls = require('./PlayersCalls');
+
+
 module.exports = function(app){
   app.route('/data')
     //testing get api call
     .get(function(req,res){
-      var response = {
-        "Test": "It is working",
-        "Description": 2
-      };
-      var data = {
-        Username: 'carlosdanna',
-        LastTimeConnected: Date.now()
-      }
-      playerCalls.insertPlayer(data);
-
-      res.send(response);
+      playerCalls.getPlayers(req,res);
     })
 
     //Testing post api call
     .post(function(req,res){
-      console.log(req.body);
-      var response = {"success": "Congratulations you made it"};
-      res.send(response);
+      playerCalls.insertPlayer(req,res);
     });
 }
